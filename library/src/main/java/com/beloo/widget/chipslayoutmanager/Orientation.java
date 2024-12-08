107c58e13c71f0e5ecf0c4827d3ed9e73309b512
Bami <codebymi@gmail.com>
2024-12-09 02:56:54 +0700

    Version 24.12.6

---
+++ library/src/main/java/com/beloo/widget/chipslayoutmanager/Orientation.java
@@ -0,0 +1,8 @@
+package com.beloo.widget.chipslayoutmanager;
+
+
+import androidx.annotation.IntDef;
+
+@IntDef({ChipsLayoutManager.HORIZONTAL, ChipsLayoutManager.VERTICAL})
+@interface Orientation {
+}
